930cbfbbaa48eca6eafb0fdd1dbfe6f65e30b50a
nagase123 <nagase0926@gmail.com>
2020-08-23 17:09:00 +0900

    登録画面作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,9 @@ export default {
       rel: 'icon',
       type: 'image/x-icon',
       href: '/favicon.ico'
+    }, {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
     }]
   },
   /*
