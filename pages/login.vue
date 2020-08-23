3ae63632c450e06d6b4013850d6ed786b2c5c385
nagase123 <nagase0926@gmail.com>
2020-08-23 14:50:27 +0900

    Googleログイン実装

--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="flex justify-center">
-    <div class="flex items-center flex-col h-48 mt-20 w-4/5 p-4 bg-white shadow rounded">
+    <div
+      class="flex items-center flex-col h-48 mt-20 w-4/5 p-4 bg-white shadow rounded"
+    >
       <div class="w-full border-b pb-2 mb-10">
         <p class="text-center">ログイン</p>
       </div>
 
-      <form class="w-full">
+      <form class="w-full" @submit.prevent="onGoogleSubmit">
         <button
           type="submit"
           class="w-full bg-google py-3 px-10 text-white rounded hover:outline-none hover:shadow-outline border"
-        >Googleでログイン</button>
+        >
+          Googleでログイン
+        </button>
       </form>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onGoogleSubmit() {
+      // FirebaseのGoogleログイン用プロバイダーを作成
+      const provider = new this.$firebase.auth.GoogleAuthProvider();
+
+      // 引数で受け取ったプロバイダーのログイン画面にリダイレクトする
+      this.$fireAuth.signInWithRedirect(provider);
+    }
+  }
+};
+</script>
