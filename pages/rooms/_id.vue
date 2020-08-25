8960158c9fc5b927eca8003e75a464652cc90217
nagase123 <nagase0926@gmail.com>
2020-08-25 22:39:56 +0900

    chatを表示

--- pages/rooms/_id.vue
+++ pages/rooms/_id.vue
@@ -1,27 +1,53 @@
 <template>
   <div class="relative">
-    <div class="px-4 pb-32"></div>
-    <form
-      @submit.prevent="onSubmit"
-      class="fixed bottom-0 bg-white w-full max-w-sm flex py-4 border-t border-gray-300"
-    >
-      <textarea
-        v-model="form.message.val"
-        name="form.body"
-        placeholder="発言してみよう"
-        class="block appearance-none w-full ml-2 py-3 px-4 rounded-lg border border-gray-400 text-darkGray focus:outline-none focus:bg-white overflow-hidden bg-blue-100"
-      ></textarea>
-      <button
-        :disabled="isValidateError"
-        :class="{'text-blue':!isValidateError}"
-        class="w-2/12 flex items-center justify-center text-gray font-semibold"
-      >送信</button>
-    </form>
+    <div class="pb-32">
+      <div v-for="chat in chats" :key="chat.id">
+        <!-- 自分の発言の場合 -->
+        <template v-if="isMyChat(chat.userId)">
+          <div class="flex my-4 justify-end">
+            <div class="bg-iceblue rounded-tr-md rounded-l-md p-4 mt-1 w-72">
+              <pre class="whitespace-pre-wrap text-sm">{{chat.body}}</pre>
+            </div>
+          </div>
+        </template>
+
+        <!-- 相手の発言の場合 -->
+        <template v-else>
+          <div class="flex my-4">
+            <div class="mr-2 mt-2 min-w-3">
+              <img :src="chat.iconImageUrl" class="rounded-full h-10 w-10" alt />
+            </div>
+            <div class="w-72">
+              <span class="text-xs">{{chat.name}}</span>
+              <div class="bg-gray-300 rounded-bl-md rounded-r-md p-4">
+                <pre class="whitespace-pre-wrap text-sm">{{chat.body}}</pre>
+              </div>
+            </div>
+          </div>
+        </template>
+      </div>
+      <form
+        @submit.prevent="onSubmit"
+        class="fixed bottom-0 bg-white w-full max-w-sm flex py-4 border-t border-gray-300"
+      >
+        <textarea
+          v-model="form.message.val"
+          name="form.body"
+          placeholder="発言してみよう"
+          class="block appearance-none w-full ml-2 py-3 px-4 rounded-lg border border-gray-400 text-darkGray focus:outline-none focus:bg-white overflow-hidden bg-blue-100"
+        ></textarea>
+        <button
+          :disabled="isValidateError"
+          :class="{'text-blue':!isValidateError}"
+          class="w-2/12 flex items-center justify-center text-gray font-semibold"
+        >送信</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapGetters, mapMutations } from "vuex";
 export default {
   middleware: ["checkAuth"],
   data() {
@@ -31,15 +57,33 @@ export default {
           val: null,
         },
       },
+      unsubscribe: null,
     };
   },
   computed: {
+    ...mapGetters("chats", ["chats"]),
     isValidateError() {
       return !this.form.message.val;
     },
   },
+  async asyncData({ store, params }) {
+    const roomId = params.id;
+    const unsubscribe = await store.dispatch("chats/subscribe", { roomId });
+    return {
+      unsubscribe,
+    };
+  },
+  destroyed() {
+    this.$store.dispatch("chats/clear");
+    if (this.unsubscribe) this.unsubscribe();
+  },
   methods: {
     ...mapMutations("alert", ["setMessage"]),
+
+    isMyChat(userId) {
+      const { uid } = this.$fireAuth.currentUser;
+      return userId === uid;
+    },
 
     async onSubmit() {
       if (this.isValidateError) return;
